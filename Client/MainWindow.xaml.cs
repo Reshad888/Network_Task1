11d9f7c688bcbfd32fde8bb506cf38fb3783cedf
Qarax_cs20@itstep.edu.az <Qarax_cs20@itstep.edu.az>
2024-09-19 01:52:53 +0300

    Add project files.

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows;
+
+namespace Client;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void SendImageButton_Click(object sender, RoutedEventArgs e)
+    {
+        string ip = IpTextBox.Text;
+        int port = int.Parse(PortTextBox.Text);
+
+        var openFileDialog = new OpenFileDialog();
+        openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+
+        if (openFileDialog.ShowDialog() == true)
+        {
+            string filePath = openFileDialog.FileName;
+            var imageBytes = File.ReadAllBytes(filePath);
+
+            try
+            {
+                using var client = new TcpClient(ip, port);
+                using var networkStream = client.GetStream();
+
+                networkStream.Write(imageBytes, 0, imageBytes.Length);
+                MessageBox.Show("Image sent successfully");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Mistake: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Server/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Server
+{
+    public partial class MainWindow : Window
+    {
+        private TcpListener listener;
+        private string serverIP = "192.168.1.151";
+        private int port = 27001;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            StartServer();
+        }
+
+        private void StartServer()
+        {
+            IPAddress iPAddress = IPAddress.Parse(serverIP);
+
+            listener = new TcpListener(iPAddress, port);
+            listener.Start();
+
+            ListenForClientsAsync();
+        }
+
+        private async Task ListenForClientsAsync()
+        {
+            while (true)
+            {
+                var client = await listener.AcceptTcpClientAsync();
+
+                await ReceiveImageAsync(client);
+            }
+        }
+
+        private async Task ReceiveImageAsync(TcpClient client)
+        {
+            using var networkStream = client.GetStream();
+            using var memoryStream = new MemoryStream();
+
+            await networkStream.CopyToAsync(memoryStream);
+            var imageBytes = memoryStream.ToArray();
+
+            Dispatcher.Invoke(() => DisplayImage(imageBytes));
+        }
+
+        private void DisplayImage(byte[] imageBytes)
+        {
+            using var ms = new MemoryStream(imageBytes);
+
+            var bitmapImage = new BitmapImage();
+
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = ms;
+            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+            bitmapImage.EndInit();
+
+            image.Source = bitmapImage;
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            listener?.Stop();
+        }
+    }
+}
